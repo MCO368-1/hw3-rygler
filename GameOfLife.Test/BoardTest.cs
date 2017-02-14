895ba96c7a1f18d4826d1027ba52da4e3e482d0b
Raphael Gal <raphaelgal@gmail.com>
2017-02-13 23:10:10 -0500

    Added some Board unit tests.

--- GameOfLife.Test/BoardTest.cs
+++ GameOfLife.Test/BoardTest.cs
@@ -7,13 +7,142 @@ using NUnit.Framework;
 
 namespace GameOfLife.Test
 {
-    class BoardTest
+    internal class BoardTest
     {
+
 
         [Test]
         public void CreateBoard()
         {
+            var board = new Board();
+
+            for (int row = 0; row < board.Cells.GetLength(0); row++)
+            {
+                for (int column = 0; column < board.Cells.GetLength(1); column++)
+                {
+                    Assert.AreEqual(false, board.Cells[row, column].IsAlive);
+                }
+            }
+
         }
 
+        [Test]
+        public void SetBlinkerPattern()
+        {
+            var board = new Board();
+            board.SetPattern(Pattern.Blinker);
+
+            Assert.True(board.Cells[9, 9].IsAlive);
+            Assert.True(board.Cells[9, 10].IsAlive);
+            Assert.True(board.Cells[9, 11].IsAlive);
+        }
+
+        [Test]
+        public void SetToadPattern()
+        {
+            var board = new Board();
+            board.SetPattern(Pattern.Toad);
+
+            Assert.True(board.Cells[9, 9].IsAlive);
+            Assert.True(board.Cells[9, 10].IsAlive);
+            Assert.True(board.Cells[9, 11].IsAlive);
+            Assert.True(board.Cells[10, 8].IsAlive);
+            Assert.True(board.Cells[10, 9].IsAlive);
+            Assert.True(board.Cells[10, 10].IsAlive);
+            
+        }
+
+        [Test]
+        public void SetBeaconPattern()
+        {
+            var board = new Board();
+            board.SetPattern(Pattern.Beacon);
+
+            Assert.True(board.Cells[9, 8].IsAlive);
+            Assert.True(board.Cells[9, 9].IsAlive);
+            Assert.True(board.Cells[10, 8].IsAlive);
+            Assert.True(board.Cells[10, 9].IsAlive);
+            Assert.True(board.Cells[11, 10].IsAlive);
+            Assert.True(board.Cells[11, 11].IsAlive);
+            Assert.True(board.Cells[12, 10].IsAlive);
+            Assert.True(board.Cells[12, 11].IsAlive);
+
+        }
+
+        [Test]
+        public void SetPulsarPattern()
+        {
+            var board = new Board();
+            board.SetPattern(Pattern.Pulsar);
+
+            Assert.True(board.Cells[4, 5].IsAlive);
+            Assert.True(board.Cells[4, 6].IsAlive);
+            Assert.True(board.Cells[4, 7].IsAlive);
+            Assert.True(board.Cells[4, 11].IsAlive);
+            Assert.True(board.Cells[4, 12].IsAlive);
+            Assert.True(board.Cells[4, 13].IsAlive);
+            Assert.True(board.Cells[6, 3].IsAlive);
+            Assert.True(board.Cells[6, 8].IsAlive);
+            Assert.True(board.Cells[6, 10].IsAlive);
+            Assert.True(board.Cells[6, 15].IsAlive);
+            Assert.True(board.Cells[7, 3].IsAlive);
+            Assert.True(board.Cells[7, 8].IsAlive);
+            Assert.True(board.Cells[7, 10].IsAlive);
+            Assert.True(board.Cells[7, 15].IsAlive);
+            Assert.True(board.Cells[8, 3].IsAlive);
+            Assert.True(board.Cells[8, 8].IsAlive);
+            Assert.True(board.Cells[8, 10].IsAlive);
+            Assert.True(board.Cells[8, 15].IsAlive);
+            Assert.True(board.Cells[9, 5].IsAlive);
+            Assert.True(board.Cells[9, 6].IsAlive);
+            Assert.True(board.Cells[9, 7].IsAlive);
+            Assert.True(board.Cells[9, 11].IsAlive);
+            Assert.True(board.Cells[9, 12].IsAlive);
+            Assert.True(board.Cells[9, 13].IsAlive);
+            Assert.True(board.Cells[11, 5].IsAlive);
+            Assert.True(board.Cells[11, 6].IsAlive);
+            Assert.True(board.Cells[11, 7].IsAlive);
+            Assert.True(board.Cells[11, 11].IsAlive);
+            Assert.True(board.Cells[11, 12].IsAlive);
+            Assert.True(board.Cells[11, 13].IsAlive);
+            Assert.True(board.Cells[12, 3].IsAlive);
+            Assert.True(board.Cells[12, 8].IsAlive);
+            Assert.True(board.Cells[12, 10].IsAlive);
+            Assert.True(board.Cells[12, 15].IsAlive);
+            Assert.True(board.Cells[13, 3].IsAlive);
+            Assert.True(board.Cells[13, 8].IsAlive);
+            Assert.True(board.Cells[13, 10].IsAlive);
+            Assert.True(board.Cells[13, 15].IsAlive);
+            Assert.True(board.Cells[14, 3].IsAlive);
+            Assert.True(board.Cells[14, 8].IsAlive);
+            Assert.True(board.Cells[14, 10].IsAlive);
+            Assert.True(board.Cells[14, 15].IsAlive);
+            Assert.True(board.Cells[16, 5].IsAlive);
+            Assert.True(board.Cells[16, 6].IsAlive);
+            Assert.True(board.Cells[16, 7].IsAlive);
+            Assert.True(board.Cells[16, 11].IsAlive);
+            Assert.True(board.Cells[16, 12].IsAlive);
+            Assert.True(board.Cells[16, 13].IsAlive);  
+        }
+
+        [Test]
+        public void SetPentadecathlonTest()
+        {
+            Board board = new Board();
+            board.SetPattern(Pattern.Pentadecathlon);
+
+            Assert.True(board.Cells[8, 7].IsAlive);
+            Assert.True(board.Cells[8, 12].IsAlive);
+            Assert.True(board.Cells[9, 5].IsAlive);
+            Assert.True(board.Cells[9, 6].IsAlive);
+            Assert.True(board.Cells[9, 8].IsAlive);
+            Assert.True(board.Cells[9, 9].IsAlive);
+            Assert.True(board.Cells[9, 10].IsAlive);
+            Assert.True(board.Cells[9, 11].IsAlive);
+            Assert.True(board.Cells[9, 13].IsAlive);
+            Assert.True(board.Cells[9, 14].IsAlive);
+            Assert.True(board.Cells[10, 7].IsAlive);
+            Assert.True(board.Cells[10, 12].IsAlive);
+        }
     }
 }
--- GameOfLife/Board.cs
+++ GameOfLife/Board.cs
@@ -5,17 +5,17 @@ namespace GameOfLife
 {
     public class Board
     {
-        private readonly Cell[,] _cells;
+        public Cell[,] Cells { get; }
 
         public Board()
         {
             const int height = 20;
             const int width = 20;
-            _cells = new Cell[height, width];
+            Cells = new Cell[height, width];
 
             for (var row = 0; row < height; row++)
                 for (var column = 0; column < width; column++)
-                    _cells[row, column] = new Cell();
+                    Cells[row, column] = new Cell();
         }
 
         public Board SetPattern(Pattern pattern)
@@ -49,110 +49,110 @@ namespace GameOfLife
 
         private void SetBlinker()
         {
-            _cells[9, 9].IsAlive = true;
-            _cells[9, 10].IsAlive = true;
-            _cells[9, 11].IsAlive = true;
+            Cells[9, 9].IsAlive = true;
+            Cells[9, 10].IsAlive = true;
+            Cells[9, 11].IsAlive = true;
         }
 
         private void SetToad()
         {
-            _cells[9, 9].IsAlive = true;
-            _cells[9, 10].IsAlive = true;
-            _cells[9, 11].IsAlive = true;
-            _cells[10, 8].IsAlive = true;
-            _cells[10, 9].IsAlive = true;
-            _cells[10, 10].IsAlive = true;
+            Cells[9, 9].IsAlive = true;
+            Cells[9, 10].IsAlive = true;
+            Cells[9, 11].IsAlive = true;
+            Cells[10, 8].IsAlive = true;
+            Cells[10, 9].IsAlive = true;
+            Cells[10, 10].IsAlive = true;
         }
 
         private void SetBeacon()
         {
-            _cells[9, 8].IsAlive = true;
-            _cells[9, 9].IsAlive = true;
-            _cells[10, 8].IsAlive = true;
-            _cells[10, 9].IsAlive = true;
-            _cells[11, 10].IsAlive = true;
-            _cells[11, 11].IsAlive = true;
-            _cells[12, 10].IsAlive = true;
-            _cells[12, 11].IsAlive = true;
+            Cells[9, 8].IsAlive = true;
+            Cells[9, 9].IsAlive = true;
+            Cells[10, 8].IsAlive = true;
+            Cells[10, 9].IsAlive = true;
+            Cells[11, 10].IsAlive = true;
+            Cells[11, 11].IsAlive = true;
+            Cells[12, 10].IsAlive = true;
+            Cells[12, 11].IsAlive = true;
         }
 
         private void SetPulsar()
         {
-            _cells[4, 5].IsAlive = true;
-            _cells[4, 6].IsAlive = true;
-            _cells[4, 7].IsAlive = true;
-            _cells[4, 11].IsAlive = true;
-            _cells[4, 12].IsAlive = true;
-            _cells[4, 13].IsAlive = true;
-            _cells[6, 3].IsAlive = true;
-            _cells[6, 8].IsAlive = true;
-            _cells[6, 10].IsAlive = true;
-            _cells[6, 15].IsAlive = true;
-            _cells[7, 3].IsAlive = true;
-            _cells[7, 8].IsAlive = true;
-            _cells[7, 10].IsAlive = true;
-            _cells[7, 15].IsAlive = true;
-            _cells[8, 3].IsAlive = true;
-            _cells[8, 8].IsAlive = true;
-            _cells[8, 10].IsAlive = true;
-            _cells[8, 15].IsAlive = true;
-            _cells[9, 5].IsAlive = true;
-            _cells[9, 6].IsAlive = true;
-            _cells[9, 7].IsAlive = true;
-            _cells[9, 11].IsAlive = true;
-            _cells[9, 12].IsAlive = true;
-            _cells[9, 13].IsAlive = true;
-            _cells[11, 5].IsAlive = true;
-            _cells[11, 6].IsAlive = true;
-            _cells[11, 7].IsAlive = true;
-            _cells[11, 11].IsAlive = true;
-            _cells[11, 12].IsAlive = true;
-            _cells[11, 13].IsAlive = true;
-            _cells[12, 3].IsAlive = true;
-            _cells[12, 8].IsAlive = true;
-            _cells[12, 10].IsAlive = true;
-            _cells[12, 15].IsAlive = true;
-            _cells[13, 3].IsAlive = true;
-            _cells[13, 8].IsAlive = true;
-            _cells[13, 10].IsAlive = true;
-            _cells[13, 15].IsAlive = true;
-            _cells[14, 3].IsAlive = true;
-            _cells[14, 8].IsAlive = true;
-            _cells[14, 10].IsAlive = true;
-            _cells[14, 15].IsAlive = true;
-            _cells[16, 5].IsAlive = true;
-            _cells[16, 6].IsAlive = true;
-            _cells[16, 7].IsAlive = true;
-            _cells[16, 11].IsAlive = true;
-            _cells[16, 12].IsAlive = true;
-            _cells[16, 13].IsAlive = true;
+            Cells[4, 5].IsAlive = true;
+            Cells[4, 6].IsAlive = true;
+            Cells[4, 7].IsAlive = true;
+            Cells[4, 11].IsAlive = true;
+            Cells[4, 12].IsAlive = true;
+            Cells[4, 13].IsAlive = true;
+            Cells[6, 3].IsAlive = true;
+            Cells[6, 8].IsAlive = true;
+            Cells[6, 10].IsAlive = true;
+            Cells[6, 15].IsAlive = true;
+            Cells[7, 3].IsAlive = true;
+            Cells[7, 8].IsAlive = true;
+            Cells[7, 10].IsAlive = true;
+            Cells[7, 15].IsAlive = true;
+            Cells[8, 3].IsAlive = true;
+            Cells[8, 8].IsAlive = true;
+            Cells[8, 10].IsAlive = true;
+            Cells[8, 15].IsAlive = true;
+            Cells[9, 5].IsAlive = true;
+            Cells[9, 6].IsAlive = true;
+            Cells[9, 7].IsAlive = true;
+            Cells[9, 11].IsAlive = true;
+            Cells[9, 12].IsAlive = true;
+            Cells[9, 13].IsAlive = true;
+            Cells[11, 5].IsAlive = true;
+            Cells[11, 6].IsAlive = true;
+            Cells[11, 7].IsAlive = true;
+            Cells[11, 11].IsAlive = true;
+            Cells[11, 12].IsAlive = true;
+            Cells[11, 13].IsAlive = true;
+            Cells[12, 3].IsAlive = true;
+            Cells[12, 8].IsAlive = true;
+            Cells[12, 10].IsAlive = true;
+            Cells[12, 15].IsAlive = true;
+            Cells[13, 3].IsAlive = true;
+            Cells[13, 8].IsAlive = true;
+            Cells[13, 10].IsAlive = true;
+            Cells[13, 15].IsAlive = true;
+            Cells[14, 3].IsAlive = true;
+            Cells[14, 8].IsAlive = true;
+            Cells[14, 10].IsAlive = true;
+            Cells[14, 15].IsAlive = true;
+            Cells[16, 5].IsAlive = true;
+            Cells[16, 6].IsAlive = true;
+            Cells[16, 7].IsAlive = true;
+            Cells[16, 11].IsAlive = true;
+            Cells[16, 12].IsAlive = true;
+            Cells[16, 13].IsAlive = true;
         }
 
         private void SetPentadecathlon()
         {
-            _cells[8, 7].IsAlive = true;
-            _cells[8, 12].IsAlive = true;
-            _cells[9, 5].IsAlive = true;
-            _cells[9, 6].IsAlive = true;
-            _cells[9, 8].IsAlive = true;
-            _cells[9, 9].IsAlive = true;
-            _cells[9, 10].IsAlive = true;
-            _cells[9, 11].IsAlive = true;
-            _cells[9, 13].IsAlive = true;
-            _cells[9, 14].IsAlive = true;
-            _cells[10, 7].IsAlive = true;
-            _cells[10, 12].IsAlive = true;
+            Cells[8, 7].IsAlive = true;
+            Cells[8, 12].IsAlive = true;
+            Cells[9, 5].IsAlive = true;
+            Cells[9, 6].IsAlive = true;
+            Cells[9, 8].IsAlive = true;
+            Cells[9, 9].IsAlive = true;
+            Cells[9, 10].IsAlive = true;
+            Cells[9, 11].IsAlive = true;
+            Cells[9, 13].IsAlive = true;
+            Cells[9, 14].IsAlive = true;
+            Cells[10, 7].IsAlive = true;
+            Cells[10, 12].IsAlive = true;
         }
 
         private void SetRandom()
         {
             var random = new Random();
-            for (var row = 0; row < _cells.GetLength(0); row++)
-                for (var column = 0; column < _cells.GetLength(1); column++)
+            for (var row = 0; row < Cells.GetLength(0); row++)
+                for (var column = 0; column < Cells.GetLength(1); column++)
                 {
                     var randomValue = random.NextDouble();
                     if (randomValue <= .2)
-                        _cells[row, column].IsAlive = true;
+                        Cells[row, column].IsAlive = true;
                 }
         }
 
@@ -160,9 +160,9 @@ namespace GameOfLife
         {
             var nextBoard = new Board();
 
-            for (var row = 0; row < _cells.GetLength(0); row++)
-                for (var column = 0; column < _cells.GetLength(0); column++)
-                    nextBoard._cells[row, column].IsAlive = GetNextState(row, column);
+            for (var row = 0; row < Cells.GetLength(0); row++)
+                for (var column = 0; column < Cells.GetLength(0); column++)
+                    nextBoard.Cells[row, column].IsAlive = GetNextState(row, column);
 
             return nextBoard;
         }
@@ -170,8 +170,8 @@ namespace GameOfLife
         private bool GetNextState(int row, int column)
         {
             var count = 0;
-            var totalRows = _cells.GetLength(0);
-            var totalColumns = _cells.GetLength(1);
+            var totalRows = Cells.GetLength(0);
+            var totalColumns = Cells.GetLength(1);
 
             for (var i = row - 1; i <= row + 1; i++)
                 for (var j = column - 1; j <= column + 1; j++)
@@ -179,24 +179,24 @@ namespace GameOfLife
                     var rowIndex = (i + totalRows) % totalRows;
                     var columnIndex = (j + totalColumns) % totalColumns;
 
-                    if (_cells[rowIndex, columnIndex].IsAlive)
+                    if (Cells[rowIndex, columnIndex].IsAlive)
                         count++;
                 }
 
             if (count == 3)
                 return true;
-            return count == 4 && _cells[row, column].IsAlive;
+            return count == 4 && Cells[row, column].IsAlive;
         }
 
         public override string ToString()
         {
             var stringBuilder = new StringBuilder();
 
-            for (var row = 0; row < _cells.GetLength(0); row++)
+            for (var row = 0; row < Cells.GetLength(0); row++)
             {
-                for (var column = 0; column < _cells.GetLength(1); column++)
+                for (var column = 0; column < Cells.GetLength(1); column++)
                 {
-                    var symbol = _cells[row, column].IsAlive ? 0 : 1;
+                    var symbol = Cells[row, column].IsAlive ? 0 : 1;
                     stringBuilder.Append(symbol + " ");
                 }
                 stringBuilder.Append("\n");
